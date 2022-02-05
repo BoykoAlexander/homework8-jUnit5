66525882e24f6709ca1f08c81a2ab02f67e1d875
b3ik2 <b3ik7636@yandex.ru>
2022-02-05 22:39:13 +0300

    add assert class from couple home work

---
+++ src/test/java/homeWork11/Assert.java
@@ -0,0 +1,12 @@
+package homeWork11;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Assert {
+
+  @Test
+  void assertTestFromHomeWork() {
+    Assertions.assertTrue(3 > 2);
+  }
+}
